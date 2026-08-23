959ff93392ee077ba780f0086ecc6b282c3c8534
yousefqb <yg.mh89@gmail.com>
2026-08-23 22:27:03 +0300

    Add files via upload

---
+++ yousef mohamed day02/Yousef Mohamed Hassan Day03/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text;
+
+namespace Yousef_Mohamed_Hassan03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region problem01
+            //string x= Console.ReadLine();
+            //int k = Convert.ToInt32(x);
+            //int y = int.Parse(x);
+
+            //Console.WriteLine(x+y+k); 
+            #endregion
+
+            #region problem02
+            // string num = Console.ReadLine();
+            //bool x= int.TryParse(num, out int y);
+            // if (x)
+            // {
+            //     global::System.Console.WriteLine(y);
+            // }
+            // else
+            // {
+            //     global::System.Console.WriteLine("error");
+            // }
+            // Console.ReadLine(); 
+            #endregion
+
+            #region problem03
+            //object O1;
+            //O1 = new object();
+            //object O2 = new object();
+
+
+            //Console.WriteLine(O1.GetHashCode());
+            //Console.WriteLine(O2.GetHashCode());
+            //O2 = O1;
+            //Console.WriteLine(O1.GetHashCode());
+            //Console.WriteLine(O2.GetHashCode()); 
+            #endregion
+
+            #region problem04
+            //object o1 = new object();
+            //object o2 = new object();
+
+            //o1 = 5;
+            //o2 = 6;
+            //o1 = o2;
+            //o1 = 10;
+            //Console.WriteLine(o2+" "+o1); 
+            #endregion
+
+            #region problem05
+            //string x= Console.ReadLine();
+            //Console.WriteLine(x.GetHashCode());
+            //x = "hi willy";
+            //Console.WriteLine(x.GetHashCode()); 
+            #endregion
+
+            #region problem06
+            //StringBuilder SB = new StringBuilder(" Hi willy ");
+            //Console.WriteLine(SB);
+            //Console.WriteLine(SB.GetHashCode());
+            //SB.Append("goooo");
+            //Console.WriteLine(SB);
+            //Console.WriteLine(SB.GetHashCode()); 
+            #endregion
+
+            #region PROBLEM08
+            //int x = 5;
+            //int y = 15;
+            //Console.WriteLine($" SUM  {x} + {y} = {x+y} "); 
+            #endregion
+
+            #region problem09
+            //StringBuilder sb = new StringBuilder("Hello World!");
+            //Console.WriteLine($"Original: {sb}");
+
+            //sb.Append(" Welcome to C#.");
+            //Console.WriteLine($"After Append: {sb}");
+
+            //sb.Replace("World", "Developer");
+            //Console.WriteLine($"After Replace: {sb}");
+
+            //sb.Insert(6, "Awesome ");
+            //Console.WriteLine($"After Insert: {sb}");
+
+            //sb.Remove(6, 8);
+            //Console.WriteLine($"After Remove: {sb}");
+            //Console.ReadLine(); 
+            #endregion
+        }
+
+    }
+}
